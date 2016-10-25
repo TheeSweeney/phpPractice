f44c0e3297190314265211b3467a33e19570dade
Brian Sweeney <brian@ajmadison.com>
2016-10-25 17:48:34 -0400

    removed unnecessary if statement in validation process

--- src/Validation/Validator.php
+++ src/Validation/Validator.php
@@ -9,38 +9,34 @@ class Validator
         $errors = [];
 
         foreach ($validation_data as $name=>$value) {
-            if (isset($_REQUEST[$name])) {
-                $rules = explode("|", $value);
+            $rules = explode("|", $value);
 
-                foreach ($rules as $rule) {
-                    $exploded = explode(":", $rule);
+            foreach ($rules as $rule) {
+                $exploded = explode(":", $rule);
 
-                    switch ($exploded[0]) {
-                      case 'min':
-                      $min = $exploded[1];
-                      if (Valid::stringType()->length($min)->Validate($_REQUEST[$name]) == false) {
-                          $errors[] = $name . " must be at least " . $min . " characters long!";
-                      }
-                      break;
+                switch ($exploded[0]) {
+                  case 'min':
+                  $min = $exploded[1];
+                  if (Valid::stringType()->length($min)->Validate($_REQUEST[$name]) == false) {
+                      $errors[] = $name . " must be at least " . $min . " characters long!";
+                  }
+                  break;
 
-                      case 'email':
-                      if (Valid::email()->Validate($_REQUEST[$name]) == false) {
-                          $errors[] = $name . " must be a valid email address";
-                      }
-                      break;
+                  case 'email':
+                  if (Valid::email()->Validate($_REQUEST[$name]) == false) {
+                      $errors[] = $name . " must be a valid email address";
+                  }
+                  break;
 
-                      case 'equalTo':
-                      if (Valid::equals($_REQUEST[$name])->Validate($_REQUEST[$exploded[1]]) == false) {
-                          $errors[] = explode("_", $exploded[1])[1] . " value does not match verification value!";
-                      }
-                      break;
+                  case 'equalTo':
+                  if (Valid::equals($_REQUEST[$name])->Validate($_REQUEST[$exploded[1]]) == false) {
+                      $errors[] = explode("_", $exploded[1])[1] . " value does not match verification value!";
+                  }
+                  break;
 
-                      default:
-                      //do nothing
-                    }
+                  default:
+                  $errors[] = "No value found";
                 }
-            } else {
-                $errors[] = "No value found";
             }
         }
         return $errors;
